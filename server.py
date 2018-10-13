da95e87e445bee33f3668994297ff4de0260bb7d
Tanishq Dubey <tanishq.dubey@gmail.com>
2018-10-13 02:41:19 -0400

    add message payload logging

--- server.py
+++ server.py
@@ -312,6 +312,7 @@ def main():
         socketio.emit('tempHeartbeat', {'temp': round(curr_temp, 1)})
         time.sleep(2)
         socketio.emit('statusHeartbeat', msg)
+        print(msg)
         socketio.emit('test', {'enabled': system.enabled});
 
         print("==============")
